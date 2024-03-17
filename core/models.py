e426b74c5e401a3a152ffb5d49816590c874e69c
Riyan Jamil <riyanjamil220@gmail.com>
2024-03-17 11:55:02 +0500

    Post like for user

--- core/models.py
+++ core/models.py
@@ -25,3 +25,9 @@ class Post(models.Model):
     def __str__(self):
         return self.user
 
+class LikePost(models.Model):
+    post_id = models.CharField(max_length = 100)
+    username = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.username
